54e1b127ea600c3457c97e0c03add2575169afcb
aabccd021 <aabccd021@gmail.com>
2025-01-29 15:15:05 +0700

    Empty

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = { self, nixpkgs, treefmt-nix }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+
+      treefmtEval = treefmt-nix.lib.evalModule pkgs {
+        projectRootFile = "flake.nix";
+        programs.prettier.enable = true;
+        programs.nixpkgs-fmt.enable = true;
+        programs.biome.enable = true;
+        programs.shfmt.enable = true;
+        settings.formatter.prettier.priority = 1;
+        settings.formatter.biome.priority = 2;
+        settings.global.excludes = [ "LICENSE" "*.ico" ];
+      };
+
+      tsc = pkgs.runCommandNoCCLocal "tsc" { } ''
+        cp -L ${./hook.ts} ./hook.ts
+        cp -L ${./tsconfig.json} ./tsconfig.json
+        ${pkgs.typescript}/bin/tsc
+        touch $out
+      '';
+
+      biome = pkgs.runCommandNoCCLocal "biome" { } ''
+        cp -L ${./biome.jsonc} ./biome.jsonc
+        cp -L ${./hook.ts} ./hook.ts
+        cp -L ${./package.json} ./package.json
+        cp -L ${./tsconfig.json} ./tsconfig.json
+        ${pkgs.biome}/bin/biome check --error-on-warnings
+        touch $out
+      '';
+
+      packages = {
+        formatting = treefmtEval.config.build.check self;
+        tsc = tsc;
+        biome = biome;
+      };
+
+      gcroot = packages // {
+        gcroot-all = pkgs.linkFarm "gcroot-all" packages;
+      };
+    in
+    {
+
+      checks.x86_64-linux = gcroot;
+
+      packages.x86_64-linux = gcroot;
+
+      formatter.x86_64-linux = treefmtEval.config.build.wrapper;
+
+      devShells.x86_64-linux.default = pkgs.mkShellNoCC {
+        buildInputs = [
+          pkgs.bun
+          pkgs.nodejs
+          pkgs.biome
+          pkgs.typescript
+        ];
+      };
+
+    };
+}
